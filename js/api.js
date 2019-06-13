5cc30df4a0f7d69cbfc2223f166e1a62219c809e
Micko Matamorosa <micko.matamorosa@boom.camp>
2019-06-13 17:58:36 +0800

    done - champions

--- js/api.js
+++ js/api.js
@@ -1,8 +1,10 @@
 const API = (function() {
-
+    const token = 'token=nS2RoUimybxL5f28GKHz9ec9GzbMWaWXH7TA7eg2oXjuTLWcPl0';
+    const cors  = 'https://cors-anywhere.herokuapp.com/';
+    
     function fetchJSON(url) {
-        return fetch("https://cors-anywhere.herokuapp.com/"+url)
-            .then(result => result.json());
+        return fetch( cors + url + '?' + token)
+              .then(result => result.json());
     }
 
     return {
--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-const allChampsURL = "https://api.pandascore.co//lol/champions?token=nS2RoUimybxL5f28GKHz9ec9GzbMWaWXH7TA7eg2oXjuTLWcPl0";
+const allChampsURL = "https://api.pandascore.co/lol/champions";
+const allItemsURL = "https://api.pandascore.co/lol/items";
+const allSkinURL = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/";
+
 const cont = document.getElementById('contents');
+const allChamps = document.getElementById('Champions');
+const allItems = document.getElementById('Items');
+
 const champ = cont.childNodes;
 const details = document.querySelector('.overlay');
 const aChampImg = document.querySelector('.active-champion-img');
@@ -15,43 +21,65 @@ const msVal = document.getElementById('ms-value');
 const arVal = document.getElementById('armor-value');
 const mrVal = document.getElementById('magic-resist-value');
 
+const closeBtn = document.querySelector('.close');
+closeBtn.addEventListener('click', () => {
+    aChampImg.removeAttribute('src');
+    details.removeAttribute('style');
+});
 
-champions(allChampsURL);
+getAllChampions(allChampsURL);
 
-function champions(URL){
+allChamps.addEventListener('click', () => getAllChampions(allChampsURL));
+allItems.addEventListener('click', () => getAllItems(allItemsURL));
+
+function getAllItems(URL){
+    cont.innerHTML = "";
+    cont.classList.remove('champs-grid');
+    cont.classList.add('items-grid');
     API.fetchJSON(URL).then(data => {
-        cont.innerHTML = data.map(reg => 
-            `<div onclick="view('${reg}')" class="champion hover"
-            style="background-image:url('${reg.big_image_url}');"
-            ><h2>${reg.name}</h2>
+        cont.innerHTML = data.map(item => 
+            `<div onclick="viewItem('${item.id}')" class="item hover">
+                <img src="${item.image_url}">
+                <h2>${item.name}</h2>
             </div>`).join('');
     });
 }
 
-function view(cData){
-    details.setAttribute('style','display:block')
-    aChampImg.setAttribute('src',cData.image_url);
-    aChampName.innerHTML = cData.name;
-    mhVal.innerHTML = `${cData.hp} (+${cData.hpperlevel} per level)`;
-    hrVal.innerHTML = `${cData.hpregen} (+${cData.hpregenperlevel} per level)`;
-    adVal.innerHTML = `${cData.attackdamage} (+${cData.attackdamageperlevel} per level)`;
-    apVal.innerHTML = `${cData.} (+${cData.} per level)`;
-    mnVal.innerHTML = `${cData.mp} (+${cData.mpperlevel} per level)`;
-    mnrVal.innerHTML= `${cData.mpregen} (+${cData.mpregenperlevel} per level)`;
-    asVal.innerHTML = `${cData.attackspeedoffset} (+${cData.attackspeedperlevel} per level)`;
-    msVal.innerHTML = cData.movespeed;
-    arVal.innerHTML = `${cData.armor} (+${cData.armorperlevel} per level)`;
-    mrVal.innerHTML = `${cData.spellblock} (+${cData.spellblockperlevel} per level)`;
+function getAllChampions(URL){
+    cont.innerHTML = "";
+    cont.classList.remove('items-grid');
+    cont.classList.add('champs-grid');
+    API.fetchJSON(URL).then(data => {
+        cont.innerHTML = data.map(reg => 
+            `<div onclick="viewChampion('${reg.id}','${reg.name}')" class="champion hover"
+            style="background-image:url('${reg.big_image_url}');"
+            ><h2>${reg.name}</h2></div>`).join('');
+    });
+}
 
+function viewChampion(champID,champName){
+    let URL = `${allChampsURL}/${champID}`;
+    let chName = champName.replace(/[' ]/g,'').replace(/&.+/g,'');
+    let skinUrl = `${allSkinURL}${chName}_0.jpg`;
+    
+    details.setAttribute('style',`display:block;background:url("${skinUrl}");`);
+    
+    API.fetchJSON(URL).then(cData => {
+        aChampImg.setAttribute('src', cData.image_url);
+        aChampName.innerHTML = cData.name;
+        mhVal.innerHTML = `${cData.hp} (+${cData.hpperlevel} per level)`;
+        hrVal.innerHTML = `${cData.hpregen} (+${cData.hpregenperlevel} per level)`;
+        adVal.innerHTML = `${cData.attackdamage} (+${cData.attackdamageperlevel} per level)`;
+        apVal.innerHTML = `${cData.attackdamage} (+${cData.attackdamageperlevel} per level)`;
+        mnVal.innerHTML = `${cData.mp} (+${cData.mpperlevel} per level)`;
+        mnrVal.innerHTML= `${cData.mpregen} (+${cData.mpregenperlevel} per level)`;
+        asVal.innerHTML = `${cData.attackspeedoffset} (+${cData.attackspeedperlevel} per level)`;
+        msVal.innerHTML =    cData.movespeed;
+        arVal.innerHTML = `${cData.armor} (+${cData.armorperlevel} per level)`;
+        mrVal.innerHTML = `${cData.spellblock} (+${cData.spellblockperlevel} per level)`;
+    });
+}
 
-
-
-
-
-
-
-
-
-
-
+function viewItem(itemID){
+    console.log(itemID);
 }
